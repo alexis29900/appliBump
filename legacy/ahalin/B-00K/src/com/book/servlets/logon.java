1100aa3a3eb23a6e6e1d3143f8759f77d2109d7c
Alexis <Alexis@LAPTOP-FQ9U0KSH>
2019-02-26 09:39:43 +0100

    logon

---
+++ legacy/ahalin/B-00K/src/com/book/servlets/logon.java
@@ -0,0 +1,47 @@
+package com.book.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.book.hibernate.HibernateUtils;
+
+
+/**
+ * @author Axel Halin
+ * 
+ * Overview: 	This servlet handles access to the main page
+ * 				of the platform. 
+ */
+@SuppressWarnings("serial")
+public class logon extends HttpServlet
+{
+	// Vue
+	public static final String VUE1 = "/WEB-INF/logon.jsp";
+
+	
+	/**
+	 * Treatment of http Get request.
+	 * 
+	 * @Override
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{	
+		HibernateUtils.initDB();
+		this.getServletContext().getRequestDispatcher(VUE1).forward(request, response);
+	}
+	
+	
+	/**
+	 * Treatment of http Post request.
+	 * 
+	 * @Override
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		this.getServletContext().getRequestDispatcher(VUE1).forward(request,response);
+	}
+}
